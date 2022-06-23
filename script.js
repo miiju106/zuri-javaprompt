d722efdf19161a5ad0e1a988e777cfb2bc2e40f2
ademiju <jemibol106@gmail.com>
2022-06-23 12:44:46 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var fullName = prompt("Enter your name");
+var height = parseFloat (prompt("Enter your height in metres"));
+var country = prompt("Enter the name of your country");
+var result = "Name: " + fullName + "\n" + "Height(m): " + height + "\n"+ "Country: " + country;
+
+    alert(result);
+
+
+
+
+
+
